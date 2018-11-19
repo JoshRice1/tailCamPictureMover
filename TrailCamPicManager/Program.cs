166894778c0dc477efe34b660171075e79396252
Josh Rice <rice.josh@gmail.com>
2018-11-19 14:19:50 -0600

    Add project files.

---
+++ TrailCamPicManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrailCamPicManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
